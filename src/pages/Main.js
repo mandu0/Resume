126d75d059db5200b1132197c0ef7773079bfe7c
manduling <dlaoswn12@naver.com>
2023-05-15 23:37:37 +0900

    main 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,28 +12,30 @@ import slid4 from "../jpg/slid4.jpg";
 import page1 from "../jpg/page1.jpg";
 import page2 from "../jpg/page2.jpg";
 import page3 from "../jpg/page3.png";
+import page4 from "../jpg/page4.png";
 import skill from "../jpg/skill.png";
-import { useRef, useState, useCallback } from "react";
+import skill2 from "../jpg/skill2.png";
+
+import { useRef, useEffect, useCallback } from "react";
 
 
 
 function Main() {
     const textObserve = useCallback((node) => {
-        const observer = new IntersectionObserver(
-          (entries) => {
-            entries.forEach(slide => {
-                if(slide.isIntersecting){
-                    console.log(slide)
-                    slide.target.style.opacity = 1;
-                    slide.target.style.transition = '1s';
-                }
-            })
-          },{threshold: 1,}
-        );
-    
+      if (node) {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((slide) => {
+            if (slide.isIntersecting) {
+              console.log(slide);
+              slide.target.style.opacity = 1;
+              slide.target.style.transition = '1s';
+            }
+          });
+        }, { threshold: 1 });
+  
         observer.observe(node);
-
-      }, []);
+      }
+    }, []);
     return(
         <main className={styles.main}>
             <section>
@@ -56,10 +58,10 @@ function Main() {
                     </div>
                     <div className={styles.IamContaiiner}>
                         <h2>저는, <br /> 프론트엔드 개발자 이민주입니다.</h2>
-                        <p>웹개발에 대하여 접하면서 여러가지 궁금증이 생기고, <br />
+                        <p>웹개발에 대해 접하면서 여러가지 궁금증이 생기고, <br />
                             그 물음표에 대하여 해결하는 매력에 빠져 개발하고 있습니다. <br />
-                            빠르게 변화하는 웹개발 트렌드에 발 맞춰 따라가려 노력하고 있습니다. <br/>
-                            앞으로도 끊임없이 성장하는 개발자가 되고싶습니다.</p>
+                            빠르게 변화하는 웹개발 트렌드에 발 맞춰 따라가려 노력할 것 입니다. <br/>
+                            앞으로도 끊임없이 성장하는 개발자가 되겠습니다.</p>
                     </div>
                 </div>
             </section>
@@ -69,18 +71,40 @@ function Main() {
                 <article>
                     <div>
                         <div className={styles.pageContainer}>
+                            <a target="_blank" href="https://radiant-cascaron-4dde14.netlify.app/"><img className={styles.pageImg} src={page2}/></a>
+                            <div ref={textObserve} className={styles.pageContent}>
+                                <h2>2022 인천시장선거 후보 홈페이지</h2>
+                                <div className={styles.pageWrap}>
+                                    <p className={styles.subContent}>내용</p>
+                                    <div>
+                                    2022년 인천시장선거 기간 배포된 사이트입니다. <br />
+                                    반응형 웹페이지 - Media Query 사용 <br />
+                                    텍스트 효과 - jQuery 이용한 글씨 애니메이션 구현<br />
+                                    페이지 배포 - 카페 24
+                                    </div>
+                                </div>
+                                <div className={styles.skillContainer2}>
+                                    <p className={styles.subContent2}>사용기술</p>
+                                    <img className={styles.skill} src={skill} />
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <div className={styles.pageContainer}>
                             <a target="_blank" href="https://calm-faloodeh-920061.netlify.app/"><img className={styles.pageImg} src={page1}/></a>                           
                              <div ref={textObserve} className={styles.pageContent}>
                                 <h2>COVID_19 국내 발생현황</h2>
                                 <div className={styles.pageWrap}>
                                     <p className={styles.subContent}>내용</p>
                                     <div>
-                                    공공데이터포털의 Open API를 활용하여 <br />
-                                    데이터를 추출한 후 자바스크립트의 비동기 처리를 <br />
-                                    async/await 이용하여 프로미스를 생성함으로써  <br />
-                                    병렬적으로 기능을 수행하여 코드를 작성하였습니다. <br />
-                                    또한 바닐라 자바스크립트로 작성하여 <br />
-                                    순수 나만의 코드를 만들어 보려고 노력한 프로젝트입니다.
+                                    Open API - 공공데이터 포털을 활용 <br />
+                                    async/await - API를 비동기 처리하여 병렬적으로 기능 수행 <br />
+                                    바닐라 자바스크립트 - observe() : div 요소에 주시 기능, <br />
+                                    setInterval : typing, 숫자 카운트 애니메이션 효과 <br />
+                                    페이지 배포 - netlify 사용, git hub와 연동하여 자동으로 수정
                                     </div>
                                 </div>
                                 <div className={styles.skillContainer2}>
@@ -94,26 +118,25 @@ function Main() {
                 <article>
                     <div>
                         <div className={styles.pageContainer}>
-                            <a target="_blank" href="https://radiant-cascaron-4dde14.netlify.app/"><img className={styles.pageImg} src={page2}/></a>
-                            <div ref={textObserve} className={styles.pageContent}>
-                                <h2>2022 인천시장선거 후보 홈페이지</h2>
+                            <a target="_blank" href="https://notice-30248.web.app/"><img className={styles.pageImg} src={page4}/></a>                           
+                             <div ref={textObserve} className={styles.pageContent}>
+                                <h2>DB게시판</h2>
                                 <div className={styles.pageWrap}>
                                     <p className={styles.subContent}>내용</p>
                                     <div>
-                                    반응형 웹페이지로 제작하기 위하여 css media Query로 <br />
-                                    홈페이지를 구현하였습니다. 데스크탑과 다르게 모바일 <br />
-                                    화면에서는 a링크들을 편하게 볼 수 있도록 하기 위하여 <br />
-                                    화면 아래쪽에 고정 배치해두었습니다.
+                                    Firestore - 실시간 데이터 베이스 확인, 컬렉션 최적화 <br />
+                                    데이터 베이스를 추가, 수정, 삭제 기능 구현 <br />
+                                    배포 - Firebase hosting
                                     </div>
                                 </div>
                                 <div className={styles.skillContainer2}>
                                     <p className={styles.subContent2}>사용기술</p>
-                                    <img className={styles.skill} src={skill} />
+                                    <img className={styles.skill2} src={skill2} />
                                 </div>
                             </div>    
                         </div>
                     </div>
-                </article>
+                </article>    
                 <article>
                     <div className={styles.wrapper}>
                         <div className={styles.pageContainer}>
@@ -123,12 +146,11 @@ function Main() {
                                 <div className={styles.pageWrap}>
                                     <p className={styles.subContent}>내용</p>
                                     <div>
-                                    기존 오래되었던 홈페이지 메인부분의 배경화면과 Nav바를 <br />
-                                    리뉴얼하는 프로젝트였습니다. 메뉴 중에서 필요 없는 <br />
-                                    부분을 수정하고 추가하는 작업이 있었으며 메인 로고 또한 <br />
-                                    직접 디자인 작업을 하여 첨부하는 과정이 있었습니다. <br />
-                                    오래된 홈페이지였기에 수정하기 어려웠던 부분도 많았지만 <br />
-                                    그만큼 더 신중하게 코드 수정을 하였던 것 같습니다. <br />
+                                    실제 리뉴얼 한 사이트 입니다. <br />
+                                    메인 이미지, 메인 글씨 수정 <br />
+                                    메뉴바 - ul/ui 태그로 수정 <br />
+                                    메인 로고 - 이미지 수정, h1 태그로 콘텐츠 분리, <br />
+                                    span 태그로 내용 안 보이게 처리하여 웹 접근성에 준수
                                     </div>
                                 </div>
                                 <div className={styles.skillContainer2}>
